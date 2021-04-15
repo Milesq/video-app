fbf4de862885875568e96758a5e830f2bb2d8402
Milesq <wmilosz88@gmail.com>
2021-04-15 14:31:09 +0200

    feat: add content to footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 function Footer() {
-  return <footer>Hello World</footer>
+  return (
+    <footer className="p-4 text-center">
+      Miłosz Wiśniewski &copy; 2021 - zadanie rekrutacyjne
+    </footer>
+  )
 }
 
 export default Footer
