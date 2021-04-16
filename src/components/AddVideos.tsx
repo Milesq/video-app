c1e8105b1a4750518a1c6a7e95177e6e473060b8
Milesq <wmilosz88@gmail.com>
2021-04-16 15:30:25 +0200

    feat: layout for add videos component

--- src/components/AddVideos.tsx
+++ src/components/AddVideos.tsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputProps,
+  InputGroupAddon,
+} from 'reactstrap'
+
+import { useSelector } from '../store'
 
 function AddVideos() {
-  return <>Hello World</>
+  const choosenTheme = useSelector(state => state.theme.theme)
+  const isDark = choosenTheme === 'dark'
+  const newMovieHandler: InputProps['innerRef'] = useRef(null)
+
+  const handleAddMovie = () => {
+    console.log(newMovieHandler.current)
+  }
+
+  return (
+    <div className="d-flex justify-content-center">
+      <InputGroup className="w-md-50">
+        <Input
+          innerRef={newMovieHandler}
+          onKeyDown={ev => ev.key === 'Enter' && handleAddMovie()}
+          className={isDark ? 'form-control--dark' : ''}
+          placeholder="Podaj link do filmu:"
+          style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
+        />
+
+        <InputGroupAddon addonType="append">
+          <Button
+            onClick={handleAddMovie}
+            color="success"
+            type="button"
+            style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+          >
+            Dodaj
+          </Button>
+        </InputGroupAddon>
+      </InputGroup>
+    </div>
+  )
 }
 
 export default AddVideos
