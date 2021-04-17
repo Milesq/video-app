e93780581cbfd96c5d2803b4d793151048ba05f1
Milesq <wmilosz88@gmail.com>
2021-04-17 19:53:48 +0200

    feat: add video sort and filter service to reducer

--- src/store/videoDisplayer.ts
+++ src/store/videoDisplayer.ts
@@ -1,18 +1,45 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 
+import { Order } from '../utils'
+
 /* eslint-disable no-unused-vars */
 export enum DiplayMode {
   List,
   Tiles,
 }
+
+export enum SortKey {
+  Date = 'date',
+  Likes = 'like',
+  Views = 'view',
+}
+
 /* eslint-enable */
+
+export interface SortBy {
+  key: SortKey | null
+  order?: Order
+}
+
+export interface Filters {
+  favorite: boolean | null
+}
 
 export interface DisplayerState {
   mode: DiplayMode
+  filterBy: Filters
+  sortBy: Required<SortBy>
 }
 
 const initialState: DisplayerState = {
   mode: DiplayMode.List,
+  filterBy: {
+    favorite: null,
+  },
+  sortBy: {
+    key: null,
+    order: Order.Desc,
+  },
 }
 
 export const slice = createSlice({
@@ -21,6 +48,18 @@ export const slice = createSlice({
   reducers: {
     changeDisplayMode(state, { payload }: PayloadAction<DiplayMode>) {
       state.mode = payload
+    },
+    setSortKey(state, { payload }: PayloadAction<SortBy>) {
+      state.sortBy = {
+        ...state.sortBy,
+        ...payload,
+      }
+    },
+    setFilter(state, { payload }: PayloadAction<Partial<Filters>>) {
+      state.filterBy = {
+        ...state.filterBy,
+        ...payload,
+      }
     },
   },
 })
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { default as ThemeContext } from './theme'
 export { default as calculateCenterClasses } from './calculateCenterClasses'
 export type { CenterProps } from './calculateCenterClasses'
+export { Order } from './order'
