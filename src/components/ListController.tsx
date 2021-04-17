dda9c0e4a071e3b29bad1604ec02fc8c83d762a8
Milesq <wmilosz88@gmail.com>
2021-04-17 20:24:01 +0200

    feat: set favorite only state to store

--- src/components/ListController.tsx
+++ src/components/ListController.tsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
 
+import { useSelector, videoDisplayer } from '../store'
 import { HearthIcon, ListIcon } from '../svg'
 
 function Controller() {
+  const dispatch = useDispatch()
+  const filters = useSelector(state => state.videoDisplayer.filterBy)
+
+  const isFavoriteOnly = filters.favorite
+
+  const changeFavoriteOnly = () => {
+    dispatch(
+      videoDisplayer.setFilter({
+        favorite: !isFavoriteOnly,
+      })
+    )
+  }
+
   return (
     <div className="d-flex justify-content-end" style={{ gap: 10 }}>
       <div>
-        <HearthIcon width="32" className="text-insta-red cursor-pointer" />
+        <HearthIcon
+          width="32"
+          onClick={changeFavoriteOnly}
+          stroke="var(--insta-red)"
+          className={`cursor-pointer ${
+            // prettier-ignore
+            isFavoriteOnly
+              ? 'text-insta-red'
+              : 'text-transparent'
+          }`}
+        />
       </div>
       <div>
         <ListIcon width="32" className="text-secondary cursor-pointer" />
