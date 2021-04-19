2d4ab4bd0ce26b6b74820b340335fca5059b3164
Milesq <wmilosz88@gmail.com>
2021-04-20 00:19:56 +0200

    feat: get data from vimeo

---
+++ src/api/getVimeoVideo.ts
@@ -0,0 +1,49 @@
+import { VideoError } from '../errors'
+
+const API = 'https://api.vimeo.com/videos/'
+
+export interface VimeoResponse {
+  error?: string
+  name: string
+  likes: string
+  link: string
+  embed: {
+    html: string
+  }
+  created_time: string
+  pictures: {
+    sizes: {
+      width: number
+      link: string
+    }[]
+  }
+}
+
+const fetchSingedReq = (url: string) =>
+  fetch(url, {
+    headers: {
+      Authorization: `Bearer ${process.env.REACT_APP_VIMEO_CLIENT_ID}`,
+    },
+  }).then(resp => resp.json())
+
+async function getVimeoVideo(id: string): Promise<VimeoResponse> {
+  const url = new URL(`${API}/${id}`)
+  url.searchParams.set('fields', 'embed,name,link,created_time,pictures')
+
+  const resp: VimeoResponse = await fetchSingedReq(url.href)
+
+  if (resp.error) {
+    return Promise.reject(VideoError.BadId)
+  }
+
+  const { total: likesCount } = await fetchSingedReq(
+    `${API}/${id}/likes?fields=total`
+  )
+
+  return {
+    ...resp,
+    likes: likesCount,
+  }
+}
+
+export default getVimeoVideo
