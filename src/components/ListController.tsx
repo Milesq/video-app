dce5b82e84688bdf243175fa7ca5ecec5e2a6189
Milesq <wmilosz88@gmail.com>
2021-04-19 12:09:28 +0200

    feat: add demo upload

--- src/components/ListController.tsx
+++ src/components/ListController.tsx
@@ -4,13 +4,23 @@ import SortByDate from './ListController/SortByDate'
 import FavoritesOnly from './ListController/FavoritesOnly'
 import TileListView from './ListController/TileListView'
 import RemoveAll from './ListController/RemoveAll'
+import UploadDemo from './ListController/UploadDemo'
 
 function Controller() {
+  const demos = [
+    'o-Psuz7u5OI',
+    'fzI9FNjXQ0o',
+    'tN5KIUNfYGA',
+    'vl6jn-DdafM',
+  ]
+
   return (
     <div className="d-flex justify-content-end" style={{ gap: 10 }}>
       <SortByDate />
       <FavoritesOnly />
       <TileListView />
+
+      <UploadDemo videos={demos} />
       <RemoveAll />
     </div>
   )
---
+++ src/components/ListController/UploadDemo.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+import { videos } from '../../store'
+import { DemoIcon } from '../../svg'
+
+interface UploadDemoProps {
+  videos: string[]
+}
+
+function UploadDemo({ videos: demoVideos }: UploadDemoProps) {
+  const dispatch = useDispatch()
+
+  const uploadDemo = () => {
+    dispatch(videos.addVideo(demoVideos))
+  }
+
+  return <DemoIcon onClick={uploadDemo} width="42" className="cursor-pointer" />
+}
+
+export default UploadDemo
