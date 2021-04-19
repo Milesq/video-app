87e7173bbfe9ad684e2f608de2a040e5fbb8f0fd
Milesq <wmilosz88@gmail.com>
2021-04-19 13:01:51 +0200

    test: add tests for clear videos

--- src/components/ListController.tsx
+++ src/components/ListController.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import demoVideos from '../utils/demoVideos'
+
 import SortByDate from './ListController/SortByDate'
 import FavoritesOnly from './ListController/FavoritesOnly'
 import TileListView from './ListController/TileListView'
@@ -7,12 +9,7 @@ import RemoveAll from './ListController/RemoveAll'
 import UploadDemo from './ListController/UploadDemo'
 
 function Controller() {
-  const demos = [
-    'o-Psuz7u5OI',
-    'fzI9FNjXQ0o',
-    'tN5KIUNfYGA',
-    'vl6jn-DdafM',
-  ]
+  const demos = demoVideos()
 
   return (
     <div className="d-flex justify-content-end" style={{ gap: 10 }}>
---
+++ src/utils/demoVideos.ts
@@ -0,0 +1,6 @@
+export default () => [
+  'o-Psuz7u5OI',
+  'fzI9FNjXQ0o',
+  'tN5KIUNfYGA',
+  'vl6jn-DdafM',
+]
