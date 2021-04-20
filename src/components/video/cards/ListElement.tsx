1edcfddc9e2ad5960bc3b58565ef54d88dde8e78
Milesq <wmilosz88@gmail.com>
2021-04-20 20:33:10 +0200

    feat: add player for list view

--- src/components/video/cards/ListElement.tsx
+++ src/components/video/cards/ListElement.tsx
@@ -1,10 +1,11 @@
-import React, { PropsWithChildren, useState } from 'react'
+import React, { PropsWithChildren, useRef, useState } from 'react'
 import { Col, ListGroupItem } from 'reactstrap'
 
 import Styles from '../../../sass/modules/icons.module.scss'
 import { useSelector } from '../../../store'
 import { DeleteIcon, HearthIcon } from '../../../svg'
 import { format, useDidChanged } from '../../../utils'
+import VideoPlayer, { VideoPlayerHandler } from '../../VideoPlayer'
 import { VideoElementProps } from '../VideoCard'
 import VideoStats from '../VideoStats'
 
@@ -16,7 +17,7 @@ function VideoListElement({
   const choosenTheme = useSelector(state => state.theme.theme)
   const isDark = choosenTheme === 'dark'
 
-  const { title, likes, views, isFavorite, uploadDate } = video
+  const { title, likes, views, isFavorite, uploadDate, embedHtml } = video
 
   const [isLiked, setIsLiked] = useState(isFavorite as boolean)
 
@@ -25,44 +26,55 @@ function VideoListElement({
   }, [isLiked])
 
   const formattedUploadDate = format.date(uploadDate)
+  const videoPlayer = useRef<VideoPlayerHandler>(null)
 
   return (
-    <Col xs="12">
-      <ListGroupItem
-        className={`d-flex justify-content-between flex-column flex-md-row align-items-center ${
-          isDark && 'bg-dark text-white'
-        }`}
-      >
-        <div className="mb-3 mb-sm-0">{title}</div>
-        <div
-          className="d-flex justify-content-around flex-wrap flex-md-nowrap w-50"
-          style={{ gap: 20 }}
+    <>
+      <VideoPlayer ref={videoPlayer} embedHtml={embedHtml} title={title} />
+      <Col xs="12">
+        <ListGroupItem
+          className={`d-flex justify-content-between flex-column flex-md-row align-items-center ${
+            isDark && 'bg-dark text-white'
+          }`}
         >
-          <div className="text-secondary d-flex align-items-center w-100">
-            <VideoStats
-              date={formattedUploadDate}
-              likes={format.number(likes)}
-              views={format.number(views || 0)}
+          <div
+            className="mb-3 mb-sm-0"
+            onClick={() => videoPlayer.current?.open()}
+          >
+            {title}
+          </div>
+          <div
+            className="d-flex justify-content-around flex-wrap flex-md-nowrap w-50"
+            style={{ gap: 20 }}
+          >
+            <div className="text-secondary d-flex align-items-center w-100">
+              <VideoStats
+                date={formattedUploadDate}
+                likes={format.number(likes)}
+                views={format.number(views || 0)}
+              />
+            </div>
+
+            <HearthIcon
+              width="32"
+              onClick={() => setIsLiked(!isLiked)}
+              stroke="var(--insta-red)"
+              className={`cursor-pointer ${
+                isFavorite
+                  ? 'text-insta-red'
+                  : `${Styles.heart} text-transparent`
+              }`}
+            />
+
+            <DeleteIcon
+              onClick={() => onDelete?.()}
+              className={`${Styles.delete} cursor-pointer`}
+              width="32"
             />
           </div>
-
-          <HearthIcon
-            width="32"
-            onClick={() => setIsLiked(!isLiked)}
-            stroke="var(--insta-red)"
-            className={`cursor-pointer ${
-              isFavorite ? 'text-insta-red' : `${Styles.heart} text-transparent`
-            }`}
-          />
-
-          <DeleteIcon
-            onClick={() => onDelete?.()}
-            className={`${Styles.delete} cursor-pointer`}
-            width="32"
-          />
-        </div>
-      </ListGroupItem>
-    </Col>
+        </ListGroupItem>
+      </Col>
+    </>
   )
 }
 
