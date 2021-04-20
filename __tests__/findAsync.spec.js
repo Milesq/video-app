f7b12b8e97c9dc26ccf442fa649236784b286e01
Milesq <wmilosz88@gmail.com>
2021-04-20 14:13:45 +0200

    feat: return predicated value instead of raw element

--- __tests__/findAsync.spec.js
+++ __tests__/findAsync.spec.js
@@ -1,26 +1,27 @@
-import findAsync from '../src/utils/findAsync'
+import findMapAsync from '../src/utils/findMapAsync'
 
 describe('findAsync', () => {
   it('returns right element', () => {
     const array = [1, 2, 3]
-    const asyncIs = a => b => Promise.resolve(a === b)
+    const asyncIs = a => b => Promise.resolve(a === b ? b : false)
 
-    expect(findAsync(array, asyncIs(3))).resolves.toBe(3)
-    expect(findAsync(array, asyncIs(1))).resolves.toBe(1)
+    expect(findMapAsync(array, asyncIs(3))).resolves.toBe(3)
+    expect(findMapAsync(array, asyncIs(1))).resolves.toBe(1)
   })
 
   it('returns right element by index', () => {
     const array = [1, 2, 3]
-    const getIndexAsync = wantedI => (_, i) => Promise.resolve(i === wantedI)
+    const getIndexAsync = wantedI => (el, i) =>
+      Promise.resolve(i === wantedI && el)
 
-    expect(findAsync(array, getIndexAsync(2))).resolves.toBe(3)
-    expect(findAsync(array, getIndexAsync(1))).resolves.toBe(3)
+    expect(findMapAsync(array, getIndexAsync(2))).resolves.toBe(3)
+    expect(findMapAsync(array, getIndexAsync(1))).resolves.toBe(2)
   })
 
   it('returns null when doesnt find', () => {
     const array = [1, 2, 3]
     const asyncFalse = () => Promise.resolve(false)
 
-    expect(findAsync(array, asyncFalse)).resolves.toBe(null)
+    expect(findMapAsync(array, asyncFalse)).resolves.toBe(null)
   })
 })
